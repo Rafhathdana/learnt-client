d116ff9c6d67dcb6338453c3b4c0fd38378836a6
MOHAMMED RAFHATH <danarafha@gmail.com>
2023-08-18 16:42:40 +0530

    added lesson area but got a bug

--- src/components/tutor/CreateLesson.jsx
+++ src/components/tutor/CreateLesson.jsx
@@ -1,5 +1,173 @@
-import React from "react";
-
-export default function CreateLesson() {
-  return <div>CreateLesson</div>;
+import { Button, Modal } from "flowbite-react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import PropTypes from "prop-types";
+import lessonSchema from "../../utils/validation/lesson.schema";
+import { PhotoIcon, XCircleIcon } from "@heroicons/react/20/solid";
+export default function CreateLesson({ course }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [error, setError] = useState(null);
+  const [fileName, setFileName] = useState(null);
+  const navigate = useNavigate();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(lessonSchema),
+  });
+  const formData = new FormData();
+  const onSubmit = (data) => {};
+  const handleFileSelect = async (e) => {};
+  const removeSelectedFile = async () => {};
+  return (
+    <>
+      <Button onClick={() => setIsOpen(!isOpen)} color="warning">
+        Add New Lesson
+      </Button>
+      <Modal
+        dismissible={true}
+        show={isOpen}
+        onClose={() => setIsOpen(!isOpen)}
+      >
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Modal.Header>
+            <span className="text-amber-500 nexa-font">
+              {course.title} -Add New Lesson
+            </span>
+          </Modal.Header>
+          <Modal.Body>
+            <div className="space-y-12">
+              <div className="border-b border-gray-900/10 pb-12">
+                <h2 className="text-base font-semibold leading-7 text-gray-900">
+                  Lesson No - {course.lessons.length + 1}
+                </h2>
+                <p className="mt-1 text-sm leading-6 text-gray-600">
+                  This information will be displayed publicly so be careful what
+                  you share.
+                </p>
+                <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                  <div className="sm:col-span-full">
+                    <label
+                      htmlFor="username"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Title
+                    </label>
+                    <div className="mt-2">
+                      <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-">
+                        <input
+                          type="text"
+                          {...register("title")}
+                          id="username"
+                          autoComplete="username"
+                          className="block flex-1 border-0 bg-transparent py-1.5 pl-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                          placeholder="lesson title"
+                        />
+                      </div>
+                      <p className="text-red-600 nexa-font text-xs mt-2 ml-1">
+                        {errors.title?.message}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="col-span-full">
+                    <label
+                      htmlFor="about"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Description
+                    </label>
+                    <div className="mt-2">
+                      <textarea
+                        name="about"
+                        id="about"
+                        rows={3}
+                        {...register("description")}
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leaading-6"
+                        defaultValue={""}
+                      />
+                    </div>
+                    <p className="text-red-600 nexa-font text-xs mt-2 ml-1">
+                      {errors.description?.message}
+                    </p>
+                    <p className="mt-3 text-sm leading-6 text-gray-600">
+                      Write a few sentences about this lesson.
+                    </p>
+                  </div>
+                  <div className="col-span-full">
+                    {!fileName && (
+                      <>
+                        <label
+                          htmlFor="file"
+                          className="block text-sm font-medium leading-6 text-gray-900"
+                        >
+                          Upload File
+                        </label>
+                        <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                          <div className="text-center">
+                            <PhotoIcon
+                              className="mx-auto h-12 w-12 text-gray-300"
+                              aria-hidden="true"
+                            />
+                            <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                              <label
+                                htmlFor="file-upload"
+                                className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                              >
+                                <span>Upload a Video</span>
+                                <input
+                                  type="file"
+                                  id="file-upload"
+                                  name="file-upload"
+                                  accept=".mp4, .heic, .mov, .pdf, .jpg, .jpeg , .avi , .wmv" // Allow specified file types
+                                  {...register("files", {
+                                    onChange: handleFileSelect,
+                                  })}
+                                  className="sr-only"
+                                />
+                              </label>
+                              <p className="pl-1">or Drag and Drop</p>
+                            </div>
+                            <p className="text-xs leading-5 text-gray-600">
+                              MP4, HEIC, MOV, AVI, WMV, PDF, JPG OR JPEG
+                            </p>
+                          </div>
+                          {errors.video && errors.video.message}
+                        </div>
+                      </>
+                    )}
+                    <span className="text-red-600">{error}</span>
+                    {fileName && (
+                      <div className="flex justify-between items-center bg-indigo-100 rounded-md mt-3 hover:bg-indigo-50">
+                        <div className="nexa-font ml-3 p-2">{fileName}</div>
+                        <div>
+                          <XCircleIcon
+                            className="w-6 mr-2 hover:cursor-pointer hover:text-red-300 hover:rotate-90 duration-200"
+                            onClick={removeSelectedFile}
+                          />
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Modal.Body>
+          <Modal.Footer className="flex justify-end">
+            <Button type="submit" onClick={() => {}}>
+              Create Lesson
+            </Button>
+            <Button color="gray" onClick={() => setIsOpen(!isOpen)}>
+              Go Back
+            </Button>
+          </Modal.Footer>
+        </form>
+      </Modal>
+    </>
+  );
 }
+CreateLesson.propTypes = {
+  course: PropTypes.object,
+};
--- src/pages/tutor/CourseLesson.jsx
+++ src/pages/tutor/CourseLesson.jsx
@@ -4,13 +4,24 @@ import SectionTitle from "../../components/common/SectionTitle";
 import HorizontalRule from "../../components/common/HorizontalRule";
 import { Badge } from "flowbite-react";
 import CreateLesson from "../../components/tutor/CreateLesson";
+import { Link, useParams } from "react-router-dom";
+import { getCourseDetailsAPI } from "../../api/tutor";
 
 export default function CourseLesson() {
+  const { id } = useParams();
   const [course, setCourse] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     setIsLoading(true);
-  });
+    getCourseDetailsAPI(id).then((response) => {
+      const course = response.data?.data;
+      console.log(course);
+      setCourse(course);
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 1000);
+    });
+  }, []);
   return (
     <>
       {isLoading ? (
@@ -25,7 +36,7 @@ export default function CourseLesson() {
           <div className="flex nexa-font p-3">
             <div className="flex-1 p-10 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
               <img
-                src={course?.thumbnailURL}
+                src={course?.thumbnail}
                 className="w-125 card ml-10 rounded"
                 alt="alternate image"
               />
@@ -45,6 +56,50 @@ export default function CourseLesson() {
               <img src="" alt="" />
               <div className="flex justify-center">
                 <CreateLesson course={course} />
+              </div>
+              {course?.lessons?.length ? (
+                <>
+                  <h1 className="text-amber-500 drop-shadow-sm  nexa-font text-start text-xl md:text-1xl ml-3 mt-10 mb-10 font-black">
+                    View Lessons
+                  </h1>
+                </>
+              ) : null}
+              <div
+                className={`flex ${
+                  course?.lessons?.length ? "justify-center" : ""
+                } flex-col  gap-3 items-center h-30 mb-5`}
+              >
+                {course?.lessons?.length ? (
+                  course.lessons.map((lesson, index) => (
+                    <Link
+                      key={lesson._id}
+                      className="flex justify-between items-center min-w-full p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+                    >
+                      <div className="rounded">
+                        <div>
+                          <span className="text-red-300 mr-2">{index + 1}</span>
+                          <span> - {lesson.title}</span>
+                        </div>
+                      </div>
+                      <div>
+                        <span>10:00</span>
+                      </div>
+                    </Link>
+                  ))
+                ) : (
+                  <div className="text-red-500 flex flex-col items-center gap-8 justify-center">
+                    <div className="mt-10">
+                      'No Lessons created for this Course'
+                    </div>
+                    <div>
+                      <img
+                        src="https://img.freepik.com/free-vector/no-data-concept-illustration_114360-536.jpg?w=2000"
+                        alt="image"
+                        className="w-132.5"
+                      />
+                    </div>
+                  </div>
+                )}
               </div>
             </div>
           </div>
